bc2aa817c61f82740cb47a5a63d445d3f9b97c14
Toke Faurby <Tokeee@gmail.com>
2017-06-15 11:22:08 +0900

    make import pretty

--- utils.py
+++ utils.py
@@ -1,7 +1,5 @@
 ## Python 2/3 compatability
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
+from __future__ import absolute_import, division, print_function
 
 import tensorflow as tf
 import numpy as np
