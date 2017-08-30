c0bca41ef507d240ab542ff816c3b52ae79432fa
Mikkel Vilstrup <mikkel@vilstrup.dk>
2017-08-30 14:32:10 +0200

    Removed inspiration

--- inspiration/CS231/assignment2/collectSubmission.sh
+++
@@ -1,2 +0,0 @@
-rm -f assignment2.zip
-zip -r assignment2.zip . -x "*.git*" "*cs231n/datasets*" "*.ipynb_checkpoints*" "*README.md" "*collectSubmission.sh" "*requirements.txt" ".env/*" "*.pyc" "*cs231n/build/*"
